ecf967d59819b4254e940ec3f7a3ab6968d370c3
Viorel <Viorel@noreply.github.com>
2023-06-30 17:17:48 +0300

    New process handling.

--- RegExpressWPFNET/RegexEngines/Boost/BoostPlugin/Matcher.cs
+++ RegExpressWPFNET/RegexEngines/Boost/BoostPlugin/Matcher.cs
@@ -35,158 +35,135 @@ namespace BoostPlugin
                     .ToArray( );
 
 
-            MemoryStream? stdout_contents;
-            string? stderr_contents;
+            using ProcessHelper ph = new ProcessHelper( GetWorkerExePath( ) );
 
-            Action<Stream> stdin_writer = s =>
+            ph.AllEncoding = EncodingEnum.Unicode;
+
+            ph.BinaryWriter = bw =>
             {
-                using( var bw = new BinaryWriter( s, Encoding.Unicode, leaveOpen: false ) )
-                {
-                    bw.Write( "m" );
-                    bw.Write( (byte)'b' );
+                bw.Write( "m" );
+                bw.Write( (byte)'b' );
 
-                    bw.Write( pattern );
-                    bw.Write( text );
+                bw.Write( pattern );
+                bw.Write( text );
 
-                    bw.Write( Enum.GetName( options.Grammar )! );
+                bw.Write( Enum.GetName( options.Grammar )! );
 
-                    // Syntax options
+                // Syntax options
 
-                    bw.Write( Convert.ToByte( options.icase ) );
-                    bw.Write( Convert.ToByte( options.nosubs ) );
-                    bw.Write( Convert.ToByte( options.optimize ) );
-                    bw.Write( Convert.ToByte( options.collate ) );
-                    bw.Write( Convert.ToByte( options.no_except ) );
-                    bw.Write( Convert.ToByte( options.no_mod_m ) );
-                    bw.Write( Convert.ToByte( options.no_mod_s ) );
-                    bw.Write( Convert.ToByte( options.mod_s ) );
-                    bw.Write( Convert.ToByte( options.mod_x ) );
-                    bw.Write( Convert.ToByte( options.no_empty_expressions ) );
+                bw.Write( Convert.ToByte( options.icase ) );
+                bw.Write( Convert.ToByte( options.nosubs ) );
+                bw.Write( Convert.ToByte( options.optimize ) );
+                bw.Write( Convert.ToByte( options.collate ) );
+                bw.Write( Convert.ToByte( options.no_except ) );
+                bw.Write( Convert.ToByte( options.no_mod_m ) );
+                bw.Write( Convert.ToByte( options.no_mod_s ) );
+                bw.Write( Convert.ToByte( options.mod_s ) );
+                bw.Write( Convert.ToByte( options.mod_x ) );
+                bw.Write( Convert.ToByte( options.no_empty_expressions ) );
 
-                    // Match options
+                // Match options
 
-                    bw.Write( Convert.ToByte( options.match_not_bob ) );
-                    bw.Write( Convert.ToByte( options.match_not_eob ) );
-                    bw.Write( Convert.ToByte( options.match_not_bol ) );
-                    bw.Write( Convert.ToByte( options.match_not_eol ) );
-                    bw.Write( Convert.ToByte( options.match_not_bow ) );
-                    bw.Write( Convert.ToByte( options.match_not_eow ) );
-                    bw.Write( Convert.ToByte( options.match_any ) );
-                    bw.Write( Convert.ToByte( options.match_not_null ) );
-                    bw.Write( Convert.ToByte( options.match_continuous ) );
-                    bw.Write( Convert.ToByte( options.match_partial ) );
-                    bw.Write( Convert.ToByte( options.match_extra ) );
-                    bw.Write( Convert.ToByte( options.match_single_line ) );
-                    bw.Write( Convert.ToByte( options.match_prev_avail ) );
-                    bw.Write( Convert.ToByte( options.match_not_dot_newline ) );
-                    bw.Write( Convert.ToByte( options.match_not_dot_null ) );
-                    bw.Write( Convert.ToByte( options.match_posix ) );
-                    bw.Write( Convert.ToByte( options.match_perl ) );
-                    bw.Write( Convert.ToByte( options.match_nosubs ) );
+                bw.Write( Convert.ToByte( options.match_not_bob ) );
+                bw.Write( Convert.ToByte( options.match_not_eob ) );
+                bw.Write( Convert.ToByte( options.match_not_bol ) );
+                bw.Write( Convert.ToByte( options.match_not_eol ) );
+                bw.Write( Convert.ToByte( options.match_not_bow ) );
+                bw.Write( Convert.ToByte( options.match_not_eow ) );
+                bw.Write( Convert.ToByte( options.match_any ) );
+                bw.Write( Convert.ToByte( options.match_not_null ) );
+                bw.Write( Convert.ToByte( options.match_continuous ) );
+                bw.Write( Convert.ToByte( options.match_partial ) );
+                bw.Write( Convert.ToByte( options.match_extra ) );
+                bw.Write( Convert.ToByte( options.match_single_line ) );
+                bw.Write( Convert.ToByte( options.match_prev_avail ) );
+                bw.Write( Convert.ToByte( options.match_not_dot_newline ) );
+                bw.Write( Convert.ToByte( options.match_not_dot_null ) );
+                bw.Write( Convert.ToByte( options.match_posix ) );
+                bw.Write( Convert.ToByte( options.match_perl ) );
+                bw.Write( Convert.ToByte( options.match_nosubs ) );
 
-                    bw.Write( Convert.ToInt16( possible_group_names.Length ) );
-                    foreach( var n in possible_group_names ) bw.Write( n );
+                bw.Write( Convert.ToInt16( possible_group_names.Length ) );
+                foreach( var n in possible_group_names ) bw.Write( n );
 
-                    bw.Write( (byte)'e' );
-                }
+                bw.Write( (byte)'e' );
             };
 
-            if( !ProcessUtilities.InvokeExe( cnc, GetWorkerExePath( ), null, stdin_writer, out stdout_contents, out stderr_contents, EncodingEnum.Unicode ) )
+            if( !ph.Start( cnc ) ) return RegexMatches.Empty;
+
+            if( !string.IsNullOrWhiteSpace( ph.Error ) ) throw new Exception( ph.Error );
+
+            var br = ph.BinaryReader;
+
+            List<IMatch> matches = new List<IMatch>( );
+            ISimpleTextGetter stg = new SimpleTextGetter( text );
+            SimpleMatch? current_match = null;
+            SimpleGroup? current_group = null;
+
+            if( br.ReadByte( ) != 'b' ) throw new Exception( "Invalid response [1]." );
+
+            bool done = false;
+
+            while( !done )
             {
-                return RegexMatches.Empty;
-            }
-
-            if( cnc.IsCancellationRequested ) return RegexMatches.Empty;
-
-            if( !string.IsNullOrWhiteSpace( stderr_contents ) ) throw new Exception( stderr_contents );
-
-            if( stdout_contents == null ) throw new Exception( "Null response" );
-
-            using( var br = new BinaryReader( stdout_contents, Encoding.Unicode ) )
-            {
-                List<IMatch> matches = new List<IMatch>( );
-                ISimpleTextGetter stg = new SimpleTextGetter( text );
-                SimpleMatch? current_match = null;
-                SimpleGroup? current_group = null;
-
-                if( br.ReadByte( ) != 'b' ) throw new Exception( "Invalid response [1]." );
-
-                bool done = false;
-
-                while( !done )
+                switch( br.ReadByte( ) )
                 {
-                    switch( br.ReadByte( ) )
-                    {
-                    case (byte)'m':
-                    {
-                        Int32 index = br.ReadInt32( );
-                        Int32 length = br.ReadInt32( );
-                        current_match = SimpleMatch.Create( index, length, stg );
-                        matches.Add( current_match );
-                        current_group = null;
-                    }
-                    break;
-                    case (byte)'g':
-                    {
-                        if( current_match == null ) throw new Exception( "Invalid response [2]." );
-                        bool success = br.ReadByte( ) != 0;
-                        Int32 index = br.ReadInt32( );
-                        Int32 length = br.ReadInt32( );
-                        string name = br.ReadString( );
-                        current_group = current_match.AddGroup( success ? index : 0, success ? length : 0, success, name );
-                    }
-                    break;
-                    case (byte)'c':
-                    {
-                        if( current_group == null ) throw new Exception( "Invalid response [3]." );
-                        Int32 index = br.ReadInt32( );
-                        Int32 length = br.ReadInt32( );
-                        current_group.AddCapture( index, length );
-                    }
-                    break;
-                    case (byte)'e':
-                        done = true;
-                        break;
-                    default:
-                        throw new Exception( "Invalid response [4]." );
-                    }
+                case (byte)'m':
+                {
+                    Int32 index = br.ReadInt32( );
+                    Int32 length = br.ReadInt32( );
+                    current_match = SimpleMatch.Create( index, length, stg );
+                    matches.Add( current_match );
+                    current_group = null;
                 }
-
-                return new RegexMatches( matches.Count, matches );
+                break;
+                case (byte)'g':
+                {
+                    if( current_match == null ) throw new Exception( "Invalid response [2]." );
+                    bool success = br.ReadByte( ) != 0;
+                    Int32 index = br.ReadInt32( );
+                    Int32 length = br.ReadInt32( );
+                    string name = br.ReadString( );
+                    current_group = current_match.AddGroup( success ? index : 0, success ? length : 0, success, name );
+                }
+                break;
+                case (byte)'c':
+                {
+                    if( current_group == null ) throw new Exception( "Invalid response [3]." );
+                    Int32 index = br.ReadInt32( );
+                    Int32 length = br.ReadInt32( );
+                    current_group.AddCapture( index, length );
+                }
+                break;
+                case (byte)'e':
+                    done = true;
+                    break;
+                default:
+                    throw new Exception( "Invalid response [4]." );
+                }
             }
+
+            return new RegexMatches( matches.Count, matches );
         }
 
 
         public static string? GetVersion( ICancellable cnc )
         {
-            MemoryStream? stdout_contents;
-            string? stderr_contents;
+            using ProcessHelper ph = new( GetWorkerExePath( ) );
 
-            Action<Stream> stdinWriter = s =>
+            ph.AllEncoding = EncodingEnum.Unicode;
+            ph.BinaryWriter = bw =>
             {
-                using( var bw = new BinaryWriter( s, Encoding.Unicode, leaveOpen: false ) )
-                {
-                    bw.Write( "v" );
-                }
+                bw.Write( "v" );
             };
 
-            if( !ProcessUtilities.InvokeExe( cnc, GetWorkerExePath( ), null, stdinWriter, out stdout_contents, out stderr_contents, EncodingEnum.Unicode ) )
-            {
-                return null;
-            }
+            if( !ph.Start( cnc ) ) return null;
 
-            if( cnc.IsCancellationRequested ) return null;
+            if( !string.IsNullOrWhiteSpace( ph.Error ) ) throw new Exception( ph.Error );
 
-            if( !string.IsNullOrWhiteSpace( stderr_contents ) ) throw new Exception( stderr_contents );
+            string version_s = ph.BinaryReader.ReadString( );
 
-            if( stdout_contents == null ) throw new Exception( "Null response" );
-
-            using( var br = new BinaryReader( stdout_contents, Encoding.Unicode ) )
-            {
-                string version_s = br.ReadString( );
-
-                return version_s;
-            }
+            return version_s;
         }
 
 
