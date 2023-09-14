d5b28efa39d3d4e3319cbe82e0736caffb8ee235
Viorel <Viorel@noreply.github.com>
2023-09-14 23:04:19 +0300

    Update Java engine: 20.0.2.

--- RegExpressWPFNET/RegexEngines/Java/JavaWorker/deps.bat
+++ RegExpressWPFNET/RegexEngines/Java/JavaWorker/deps.bat
@@ -1,3 +1,3 @@
 @rem TODO: adjust the JDK_ROOT variable; See "Readme".
-@set JDK_ROOT=T:\Teste\Java\jdk-20.0.1
+@set JDK_ROOT=T:\Teste\Java\jdk-20.0.2
 "%JDK_ROOT%\bin\jdeps" JavaWorker.class
--- RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.bat
+++ RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.bat
@@ -1,3 +1,3 @@
 @rem TODO: adjust the JDK_ROOT variable; See "Readme".
-@set JDK_ROOT=T:\Teste\Java\jdk-20.0.1
+@set JDK_ROOT=T:\Teste\Java\jdk-20.0.2
 "%JDK_ROOT%\bin\jlink" --add-modules java.base --output JRE-min --strip-debug --no-man-pages --no-header-files --compress=0
