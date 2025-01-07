5fe223f2d50ab3fec01a30dbe6c37333283c047a
Viorel <Viorel@noreply.github.com>
2025-01-07 23:48:57 +0200

    Update RE2 engine: 2024-07-02 ("main" branch only).

---
+++ RegExpressWPFNET/RegexEngines/RE2/AbseilStaticLib/AbseilStaticLib.cpp
@@ -0,0 +1,9 @@
+// AbseilStaticLib.cpp : Defines the functions for the static library.
+//
+
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnAbseilStaticLib()
+{
+}
