48ca6652be89c6a16cd910c268eca6591f424a79
Viorel <Viorel@noreply.github.com>
2025-06-29 22:12:43 +0300

    Update Java engines: 24.0.1.

--- RegExpressWPFNET/RegexEngines/Java/JavaWorker/deps.bat
+++ RegExpressWPFNET/RegexEngines/Java/JavaWorker/deps.bat
@@ -1,3 +1,4 @@
 @rem TODO: adjust the JDK_ROOT variable; See "Readme".
-@set JDK_ROOT=T:\Limbaje\jdk-23.0.2
+@set JDK_ROOT=T:\Limbaje\jdk-24.0.1
 "%JDK_ROOT%\bin\jdeps" JavaWorker.class
+"%JDK_ROOT%\bin\jdeps" RE2JWorker.class
--- RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.bat
+++ RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.bat
@@ -1,3 +1,3 @@
 @rem TODO: adjust the JDK_ROOT variable; See "Readme".
-@set JDK_ROOT=T:\Limbaje\jdk-23.0.2
+@set JDK_ROOT=T:\Limbaje\jdk-24.0.1
 "%JDK_ROOT%\bin\jlink" --add-modules java.base --output JRE-min --strip-debug --no-man-pages --no-header-files --compress=zip-0
