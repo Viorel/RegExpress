b408ddf1c9a8938e37a465007f903fec3571214d
Viorel <Viorel@noreply.github.com>
2026-08-23 19:32:20 +0300

    Update JRE.

--- RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.cmd
+++ RegExpressWPFNET/RegexEngines/Java/JavaWorker/makejre.cmd
@@ -1,3 +1,4 @@
 @rem TODO: adjust the JDK_ROOT variable; See "Readme".
 @set JDK_ROOT=T:\Limbaje\jdk-26.0.2.1
-"%JDK_ROOT%\bin\jlink" --add-modules java.base --output JRE-min --strip-debug --no-man-pages --no-header-files --compress=zip-0
+"%JDK_ROOT%\bin\jlink" --add-modules java.base,java.logging --output JRE-min --strip-debug --no-man-pages --no-header-files --compress=zip-0
+@rem java.logging needed for Reggie
