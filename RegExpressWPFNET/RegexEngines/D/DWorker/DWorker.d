3028bfcd6fd3dbe5a78beb7e2113d2a66bb5e724
Viorel <Viorel@noreply.github.com>
2026-09-23 22:08:07 +0300

    Fix improper format of D errors.

--- RegExpressWPFNET/RegexEngines/D/DWorker/DWorker.d
+++ RegExpressWPFNET/RegexEngines/D/DWorker/DWorker.d
@@ -9,125 +9,125 @@ import std.range;
 
 void main()
 {
-	try
-	{
-		debug 
-		{ 
-			const bool is_debug = true; 
-		}
-		else
-		{
-			const bool is_debug = false;
-		}
+    try
+    {
+        debug 
+        { 
+            const bool is_debug = true; 
+        }
+        else
+        {
+            const bool is_debug = false;
+        }
 
-		string s;
-		stdin.readf("%s", s);
+        string s;
+        stdin.readf("%s", s);
 
-		JSONValue json_value = parseJSON(s);
+        JSONValue json_value = parseJSON(s);
 
-		const(JSONValue*) command_j = "c" in json_value;
-		const string command = command_j == null ? "" : command_j.str;
+        const(JSONValue*) command_j = "c" in json_value;
+        const string command = command_j == null ? "" : command_j.str;
 
-		if( command == "v")
-		{
-			string v = format("%s.%03s", version_major, version_minor);
+        if( command == "v")
+        {
+            string v = format("%s.%03s", version_major, version_minor);
 
-			JSONValue result = JSONValue(["version" : v]);
+            JSONValue result = JSONValue(["version" : v]);
 
-			writeln(result.toString());
+            writeln(result.toString());
 
-			return;
-		}
+            return;
+        }
 
-		if( command == "m" || command == "")
-		{
-			const string pattern = json_value["p"].str;
-			const string text = json_value["t"].str;
-			const(JSONValue*) flags_j = "f" in json_value;
-			const string flags = flags_j == null ? "" : flags_j.str;
+        if( command == "m" || command == "")
+        {
+            const string pattern = json_value["p"].str;
+            const string text = json_value["t"].str;
+            const(JSONValue*) flags_j = "f" in json_value;
+            const string flags = flags_j == null ? "" : flags_j.str;
 
-			auto re = regex(pattern, flags);
+            auto re = regex(pattern, flags);
 
-			JSONValue[] names;
+            JSONValue[] names;
 
-			foreach(name; re.namedCaptures)
-			{
-				names ~= JSONValue(name);
-			}
+            foreach(name; re.namedCaptures)
+            {
+                names ~= JSONValue(name);
+            }
 
-			JSONValue[] matches;
-			JSONValue[] empty_array0;
-			JSONValue empty_array = JSONValue(empty_array0);
+            JSONValue[] matches;
+            JSONValue[] empty_array0;
+            JSONValue empty_array = JSONValue(empty_array0);
 
-			foreach(match; std.regex.matchAll(text, re))
-			{
-				// ('match' is 'std.regex.Captures')
+            foreach(match; std.regex.matchAll(text, re))
+            {
+                // ('match' is 'std.regex.Captures')
 
-				if( match.empty) continue;
+                if( match.empty) continue;
 
-				JSONValue[] groups;
+                JSONValue[] groups;
 
-				foreach( capture; match) // "groups"
-				{
-					// ( 'capture' is 'string')
+                foreach( capture; match) // "groups"
+                {
+                    // ( 'capture' is 'string')
 
-					if( capture == null) // failed?
-					{
-						groups ~= empty_array;
-					}
-					else
-					{
-						// See: https://forum.dlang.org/post/xdvjbcgvnnoxbryekawn@forum.dlang.org
+                    if( capture == null) // failed?
+                    {
+                        groups ~= empty_array;
+                    }
+                    else
+                    {
+                        // See: https://forum.dlang.org/post/xdvjbcgvnnoxbryekawn@forum.dlang.org
 
-						groups ~= JSONValue([capture.ptr - text.ptr, capture.length]);
-					}
-				}
+                        groups ~= JSONValue([capture.ptr - text.ptr, capture.length]);
+                    }
+                }
 
-				JSONValue[] named_groups;
+                JSONValue[] named_groups;
 
-				foreach(name; re.namedCaptures)
-				{
-					auto val = match[name];
+                foreach(name; re.namedCaptures)
+                {
+                    auto val = match[name];
 
-					if( val.empty) 
-					{
-						named_groups ~= JSONValue([ -1, 0 ]);
-					}
-					else
-					{
-						named_groups ~= JSONValue([ val.ptr - text.ptr, val.length ]);
-					}
-				}
+                    if( val.empty) 
+                    {
+                        named_groups ~= JSONValue([ -1, 0 ]);
+                    }
+                    else
+                    {
+                        named_groups ~= JSONValue([ val.ptr - text.ptr, val.length ]);
+                    }
+                }
 
-				auto const i = match.hit.ptr - text.ptr;
+                auto const i = match.hit.ptr - text.ptr;
 
-				JSONValue one = [ 
-					"i": JSONValue(i),
-					"g": JSONValue(groups),
-					"n": JSONValue(named_groups)
-				];
+                JSONValue one = [ 
+                    "i": JSONValue(i),
+                    "g": JSONValue(groups),
+                    "n": JSONValue(named_groups)
+                ];
 
-				matches ~= one;
-			}
+                matches ~= one;
+            }
 
-			JSONValue result = [
-				"names": names,
-				"matches": matches
-			];
+            JSONValue result = [
+                "names": names,
+                "matches": matches
+            ];
 
-			writeln(toJSON(result, /*pretty:=*/ is_debug ));
+            writeln(toJSON(result, /*pretty:=*/ is_debug ));
 
-			return;
-		}
+            return;
+        }
 
-		stderr.writefln("Unsupported command: '%s'", command);
-	}
-	catch (RegexException exc)
-	{
-		stderr.writeln(exc.message());
-	}
-	catch (Exception exc)
-	{
-		stderr.writeln(exc);
-	}
+        stderr.writefln("Unsupported command: '%s'", command);
+    }
+    catch (RegexException exc)
+    {
+        stderr.writeln(exc.message());
+    }
+    catch (Exception exc)
+    {
+        stderr.writeln(exc.message());
+    }
 }
